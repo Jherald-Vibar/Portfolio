e09ce459b188a2444ce8c45fa0e1e3d546e19729
Jherald-Vibar <vibar_jherald@spcc.edu.ph>
2026-07-10 14:59:06 +0800

    Removing in Router Jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import ProjectDetails from "./components/views/ProjectDetails.jsx";
-import ResumeViewer from "./components/views/ResumeViewer.jsx";
+
+
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />
-    },
-    {
-        path: '/resume',
-        element: <ResumeViewer/>
     },
     {
         path: '/project/:project',
