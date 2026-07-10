2e1ba19d3cff5e43e4700e78915fbba5ab4d9959
Jherald-Vibar <vibar_jherald@spcc.edu.ph>
2026-07-10 15:03:14 +0800

    Fixing Capitalization

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import ProjectDetails from "./components/views/ProjectDetails.jsx";
-
+import ResumeViewer from "./components/views/Resumeviewer.jsx";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />
+    },
+    {
+        path: '/resume',
+        element: <Resumeviewer/> 
     },
     {
         path: '/project/:project',
